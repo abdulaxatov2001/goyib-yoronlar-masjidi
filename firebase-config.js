bf56259044bd28ea732e0a8e606448d4d549e91c
macbook_uz <azizbekabdulaxatov2001@gmail.com>
2026-08-07 08:03:58 +0500

    vaqt togrilandi

--- firebase-config.js
+++ firebase-config.js
@@ -4,11 +4,12 @@
 //  Loyiha sozlamalari > Umumiy > "Veb ilovangiz" bo'limidan
 // ============================================================
 const firebaseConfig = {
-    apiKey:            "SIZNING_API_KALITINGIZ",
-    authDomain:        "LOYIHA_ID.firebaseapp.com",
-    databaseURL:       "https://LOYIHA_ID-default-rtdb.firebaseio.com",
-    projectId:         "LOYIHA_ID",
-    storageBucket:     "LOYIHA_ID.appspot.com",
-    messagingSenderId: "RAQAMINGIZ",
-    appId:             "ILOVANGIZ_ID"
+    apiKey: "AIzaSyDjU4_Bt8gzbYTwYvdFz37LSOJ6rAnYxhA",
+    authDomain: "goyib-yoronlar-masjidi.firebaseapp.com",
+    databaseURL: "https://goyib-yoronlar-masjidi-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "goyib-yoronlar-masjidi",
+    storageBucket: "goyib-yoronlar-masjidi.firebasestorage.app",
+    messagingSenderId: "607943091616",
+    appId: "1:607943091616:web:1aa14cb0c530cd78b270c5",
+    measurementId: "G-S4HY09VZSJ"
 };
